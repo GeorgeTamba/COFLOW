68df0dda648e3ea6d53d9edeb0990579992ed3a7
George Tamba <157567948+GeorgeTamba@users.noreply.github.com>
2026-06-22 17:51:50 +0700

    fix login lobby scene ui input field

--- Assets/Scripts/CustomKeypad.cs
+++ Assets/Scripts/CustomKeypad.cs
@@ -5,17 +5,23 @@ public class CustomKeypad : MonoBehaviour
 {
     [Header("UI References")]
     // Drag your UI Text or InputField here in the Unity Inspector!
-    public TMP_Text displayField; 
+    public TMP_Text displayField;
+    public GameObject validatingText;
+    public TMP_Text numLineText;
 
     [Header("Logic References")]
     // Drag the GameObject holding your VRLoginManager here!
-    public VRLoginManager loginManager; 
+    public VRLoginManager loginManager;
 
     // The secret string holding what the player is typing
     private string currentInput = "";
     // Access codes are usually 6 digits, let's enforce a limit!
-    private int maxDigits = 6; 
+    private int maxDigits = 6;
 
+    private void Awake()
+    {
+        validatingText.SetActive(false);
+    }
     // 1. Connect this to your 0-9 Buttons!
     // In the Unity Inspector OnClick(), type the number in the little box (e.g., "1", "2")
     public void ButtonPress_Number(string numberString)
@@ -41,17 +47,18 @@ public class CustomKeypad : MonoBehaviour
     // 3. Connect this to your Enter Button!
     public void ButtonPress_Enter()
     {
-        if (currentInput.Length > 0)
-        {
-            Debug.Log("Sending code from Keypad: " + currentInput);
-            displayField.text = "Validating...";
-            
-            // Hand the code over to the big API manager we built earlier!
-            loginManager.OnSubmitCode(currentInput);
-            
-            // Clear the keypad for the next try
-            currentInput = ""; 
-        }
+        //if (currentInput.Length > 0)
+        //{
+        Debug.Log("Sending code from Keypad: " + currentInput);
+        numLineText.text = "";
+        validatingText.SetActive(true);
+
+        //// Hand the code over to the big API manager we built earlier!
+        //loginManager.OnSubmitCode(currentInput);
+
+        //// Clear the keypad for the next try
+        //currentInput = ""; 
+        //}
     }
 
     // Updates the screen so the player sees what they typed
